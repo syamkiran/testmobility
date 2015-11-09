820d23f3a119ba77289669f81ee34c8181c60318
Syam <ysyamkiran@gmail.com>
2015-11-09 12:43:24 -0600

    test files

---
+++ lang_en.rb
@@ -0,0 +1,18 @@
+module LocalizationEn
+
+  Messages =
+  {
+    :error_description => "An error has occurred.",
+    :error_title => "Error",
+    :error_message => "An issue occurred while trying to save a(n) %s.\n%s",
+    :conflict_title => "Conflict",
+    :conflict_message => "A conflict was detected while updating a(n) %s. Your changes have been lost. Please try again.",
+    :notification_title => "Notification",
+    :alert_title => "Alert",
+    :quote_approval_status => "Customer Approved",
+    :lost_quote   => "Lost",
+    :abandoned_quote => "Abandoned"
+  } #Messages
+  
+
+end
---
+++ lang_fr.rb
@@ -0,0 +1,20 @@
+#!/bin/env ruby
+# encoding: utf-8
+module LocalizationFr
+
+  Messages =
+  {
+    :error_description => "An error has occurred.",
+    :error_title => "Error",
+    :error_message => "An issue occurred while trying to save a(n) %s.\n%s",
+    :conflict_title => "Conflict",
+    :conflict_message => "A conflict was detected while updating a(n) %s. Your changes have been lost. Please try again.",
+    :notification_title => "Notification",
+    :alert_title => "Alert",
+    :quote_approval_status => "Autorisé par client",
+    :lost_quote   => "Perdu",
+    :abandoned_quote => "Abandonné"
+  } #Messages
+  
+
+end
